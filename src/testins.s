da5500a29456fd7badb9eccbe7f496938f8ece6c
aspicube <yinyanzhao@outlook.com>
2019-04-01 19:50:07 +0800

    add new files

---
+++ src/testins.s
@@ -0,0 +1,29 @@
+#function name: testins parameter(s) number: 2 
+#This function is to repeat some load/store instructions
+#   to acquire their power traces for side-channel attack.
+
+
+.syntax unified
+.cpu cortex-m4
+.global testins
+.type testins %function
+.align 2 
+# void testins(uint32_t *addr, uint32_t length)
+testins:
+#push {lr}
+nop
+sub r2, r2, r2
+sub r3, r3, r3
+nop
+ldr r2, [r0, #0]
+nop
+str r3, [r0, #8]
+nop
+str r2, [r0, #8]
+nop
+ldr r2, [r0, #4]
+nop
+str r2, [r0, #8]
+nop
+#pop {pc}
+bx lr
